a48e6ca9a2a18b04f1daa328e9ae06dc9e1ceebb
Nathanael Grier <nathanael.grier@smoothstack.com>
2021-12-06 14:42:21 -0600

    services javadoc'd

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -7,12 +7,21 @@ import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * the custom http service has methids built specifically for the unique requests each service may be expecting
+ */
 export class HttpService {
 
   private newId: any = '';
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * the getHeaders method returns properly packaged headers for http requests
+   * 
+   * @returns The http headers
+   */
   getHeaders() {
     return {
       headers: new HttpHeaders({
@@ -22,6 +31,14 @@ export class HttpService {
     };
   }
 
+  /**
+   * The login method sends the orioerly packaged login request
+   * 
+   * @param email The email to send
+   * @param password The password to send 
+   * 
+   * @returns The server response to the login request
+   */
   login(email: string, password: string): Observable<HttpResponse<any>> {
     const url: string = `${environment.baseUrl}${environment.authEndpoint}`
 
@@ -45,18 +62,49 @@ export class HttpService {
       });
   }
 
+  /**
+   * The creditCheck method sends all the information necessary to perform a credit check when applying for a loan.
+   * 
+   * @param url The credit check url to use
+   * @param body the LoanType to run sa credit check for
+   * 
+   * @returns The server response from the credit check request 
+   */
   creditCheck(url: string, body: LoanType) {
     return this.http.post(url, body, this.getHeaders());
   }
 
+  /**
+   * The getAll method is used for all requests that retrieve a list for a table
+   * 
+   * @param url The url to get all from
+   * 
+   * @returns The server response from the getAll request
+   */
   getAll(url: string) {
     return this.http.get(url, this.getHeaders());
   }
 
+  /**
+   * The getById method gets an individual entity by its unique UUID id
+   * 
+   * @param url the url to get one from
+   * 
+   * @returns The server response from the getById request
+   */
   getById(url: string) {
     return this.http.get(url, this.getHeaders());
   }
 
+  /**
+   * The getNewUUID method isa used when requesting a new entity to make. The front end requestsd the server create and save an empty entity
+   * for it to work on and eventually save to the database 
+   * 
+   * @param url The url to get a new entity from
+   * @param id The id for requesting something that exists. Optional
+   * 
+   * @returns The server response from the getNewUUID request
+   */
   async getNewUUID(url: string, id?: string): Promise<any> {
     console.log('inbound url: ', url)
     if (id) {
@@ -87,62 +135,38 @@ export class HttpService {
     return this.newId;
   }
 
-  getAccounts(page: number, size: number, sort?: string, dir?: string, search?: string) {
-    let query = `${environment.baseUrl}${environment.accountsEndpoint}/all?pageNum=${encodeURIComponent(page)}&pageSize=${encodeURIComponent(size)}`;
-    if (sort === undefined && dir === undefined) {
-      sort = "id"; dir = "asc";
-    }
-    if (search === undefined) {
-      search = "";
-    }
-    if (sort !== undefined && dir !== undefined) {
-      query += `&sortName=${encodeURIComponent(sort)}&sortDir=${encodeURIComponent(dir)}`;
-    }
-    if (search !== '') {
-      query += `&search=${encodeURIComponent(search)}`;
-    }
-    console.log('Outbound Query: ', query);
-    return this.http.get(query, this.getHeaders() );
-  }
-
-  getLoans(page: number, size: number, sort?: string, dir?: string, search?: string) {
-    let query = `${environment.baseUrl}${environment.loansEndpoint}?pageNum=${encodeURIComponent(page)}&pageSize=${encodeURIComponent(size)}`;
-    if (sort === undefined && dir === undefined) {
-      sort = "id"; dir = "asc";
-    }
-    if (search === undefined) {
-      search = "";
-    }
-    if (sort !== undefined && dir !== undefined) {
-      query += `&sortName=${encodeURIComponent(sort)}&sortDir=${encodeURIComponent(dir)}`;
-    }
-    if (search !== '') {
-      query += `&search=${encodeURIComponent(search)}`;
-    }
-    console.log('Outbound Query: ', query);
-    return this.http.get(query, this.getHeaders() );
-  }
-
-  getUsers(page: number, size: number, sort?: string, asc?: boolean, search?: string) {
-    let query = `${environment.baseUrl}${environment.usersEndpoint}?page=${encodeURIComponent(page)}&size=${encodeURIComponent(size)}`;
-    if (sort !== undefined) {
-      query += `&sort=${encodeURIComponent(sort)}&asc=${encodeURIComponent(!!asc)}`;
-    }
-    if (search !== undefined) {
-      query += `&search=${encodeURIComponent(search)}`;
-    }
-    return this.http.get(query, this.getHeaders() );
-  }
-
+/**
+ * The deleteById method sends a delete request for an individual entity
+ * 
+ * @param url Tye url to delete by
+ * 
+ * @returns the server response from the deleteById request
+ */
   deleteById(url: string) {
     return this.http.delete(url, this.getHeaders());
   }
 
+  /**
+   * The create method semds a new entity to the database to save.
+   * 
+   * @param url The url to send a create request to
+   * @param obj The entity to create
+   * 
+   * @returns the server response from the create request
+   */
   create(url: string, obj: any) {
     console.log('create called')
     return this.http.post(url, obj, this.getHeaders());
   }
 
+  /**
+   * The update method sends an update request for an individual entity
+   * 
+   * @param url The url to send the update request to
+   * @param obj The entity to update
+   * 
+   * @returns the server response from the update request 
+   */
   update(url: string, obj: any) {
     console.log('update called')
     console.log('update object: ', obj)
